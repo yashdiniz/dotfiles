816803704cf68eca8255b20c840b5329b79e43ad
yashdiniz <26359876+yashdiniz@users.noreply.github.com>
2022-10-08 23:30:54 +0530

    :memo: Attempting init.lua for nvim

--- setup.sh
+++ setup.sh
@@ -1,25 +1,5 @@
-# add .bash_aliases to the home directory, where it should be.
-ln -s ~/linux-customizations/.bash_aliases ~/
-# add .vimrc to the home directory, where it shoudld be.
-ln -s ~/linux-customizations/.vimrc ~/.config/nvim/init.vim
-# add .tmux.conf to the home directory, where it should be.
-ln -s ~/linux-customizations/.tmux.conf ~/.tmux.conf
-
-mkdir -p ~/.config/alacritty
-
-# add alacritty.yml to the .config directory, where it could be.
-ln -s ~/linux-customizations/alacritty.yml ~/.config/alacritty/alacritty.yml
-
-# adding vundle to neovim
-# mkdir -p ~/.config/nvim/bundle
-# git clone https://github.com/VundleVim/Vundle.vim.git ~/.config/nvim/bundle/Vundle.vim
-
-# adding vim-Plug to neovim
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim \
-    --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-# installing alacritty
-git clone https://github.com/alacritty/alacritty.git
+#!/bin/bash
+# This script helps me setup my personal configs on a new linux machine
 
 # Install latest nodejs (required for coc.nvim)
 if [ ! -x "$(command -v node)" ]; then
@@ -28,3 +8,19 @@ if [ ! -x "$(command -v node)" ]; then
     # Or use package manager, e.g.
     # sudo apt-get install nodejs
 fi
+
+# adding vim-Plug to neovim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim \
+    --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# alacritty
+git clone https://github.com/alacritty/alacritty.git alacritty_git/
+
+# add files to the home directory
+ln -s ~/linux-customizations/.bash_aliases ~/.bash_aliases
+ln -s ~/linux-customizations/.tmux.conf ~/.tmux.conf
+
+# add folders to the .config directory
+mkdir -p ~/.config
+ln -s ~/linux-customizations/alacritty ~/.config/alacritty
+ln -s ~/linux-customizations/nvim ~/.config/nvim
