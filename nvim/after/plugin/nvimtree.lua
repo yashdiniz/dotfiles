b1d4108112b5d7961369bf6837dc4f711f37108a
yashdiniz <26359876+yashdiniz@users.noreply.github.com>
2022-12-21 18:29:28 +0530

    :memo: `NvimTreeRefresh` on `BufWritePre`

--- nvim/after/plugin/nvimtree.lua
+++ nvim/after/plugin/nvimtree.lua
@@ -101,6 +101,13 @@ vim.g.nvimtree_side = options.view.side
 
 vim.keymap.set('n', '<C-n>', '<cmd>NvimTreeToggle<CR>', { desc = 'Toggle NvimTree' })
 vim.keymap.set('n', '<leader>f', '<cmd>NvimTreeFocus<CR>', { desc = '[f]ocus NvimTree' })
-vim.keymap.set('n', '<leader>r', '<cmd>NvimTreeRefresh<CR>', { desc = '[r]efresh NvimTree' })
+
+vim.api.nvim_create_autocmd('BufWritePre', {
+  pattern = '*',
+  callback = function()
+    print('BufWritePre')
+    vim.cmd [[NvimTreeRefresh]]
+  end,
+})
 
 nvimtree.setup(options)
