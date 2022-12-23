667b415205b20126b618a2d662374f16db260160
yashdiniz <26359876+yashdiniz@users.noreply.github.com>
2022-12-23 13:32:08 +0530

    🎅 `[[ raw strings ]]`

--- nvim/after/plugin/copilot.lua
+++ nvim/after/plugin/copilot.lua
@@ -1,4 +1,4 @@
 -- remove the default keymap to tab
 vim.g.copilot_no_tab_map = true
 -- use <C-j>
-vim.keymap.set('i', '<C-j>', 'copilot#Accept("\n")', {silent = true, expr = true, script = true})
+vim.keymap.set('i', '<C-j>', [[copilot#Accept("<CR>")]], {silent = true, expr = true, script = true})
--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,2 +1,2 @@
 vim.keymap.set('c', 'ga', "Git add ", { desc = '[G]it [a]dd'})
-vim.keymap.set('c', 'gc', "<cmd>Git commit -S<CR>", { desc = '[G]it signed [c]ommit' })
+vim.keymap.set('c', 'gc', [[<cmd>Git commit -S<CR>]], { desc = '[G]it signed [c]ommit' })
