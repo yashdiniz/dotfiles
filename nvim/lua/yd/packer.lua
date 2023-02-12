ab85251468e69fa5c4043d44ec485e729ac5dcd9
yashdiniz <26359876+yashdiniz@users.noreply.github.com>
2023-02-12 22:18:47 +0530

    :memo: Neovim lsp autocompletion attempt

--- nvim/lua/yd/packer.lua
+++ nvim/lua/yd/packer.lua
@@ -60,10 +60,10 @@ require('packer').startup(function(use)
 
       -- Autocompletion
       {'hrsh7th/nvim-cmp'},
+      {'hrsh7th/cmp-nvim-lsp'},
       {'hrsh7th/cmp-buffer'},
       {'hrsh7th/cmp-path'},
       {'saadparwaiz1/cmp_luasnip'},
-      {'hrsh7th/cmp-nvim-lsp'},
       {'hrsh7th/cmp-nvim-lua'},
       {
         'windwp/nvim-autopairs',
