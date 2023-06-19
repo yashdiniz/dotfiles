8b5c819327bd1cce0cecd1748339a24086dadb80
Yash Diniz <26359876+yashdiniz@users.noreply.github.com>
2023-06-19 16:05:33 +0530

    :memo: don't escape after suggestion.

--- nvim/after/plugin/copilot.lua
+++ nvim/after/plugin/copilot.lua
@@ -1,4 +1,5 @@
+-- use <C-j>
+-- can't use in normal mode since the function only works in insert mode
+vim.keymap.set('i', '<C-j>', [[copilot#Accept('')]], { silent = true, expr = true, script = true })
 -- remove the default keymap to tab
 vim.g.copilot_no_tab_map = true
--- use <C-j>
-vim.keymap.set('i', '<C-j>', [[copilot#Accept("<CR>")]], {silent = true, expr = true, script = true})
