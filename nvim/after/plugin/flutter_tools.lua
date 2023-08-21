1ad8f7aec8f29fcb8c49ae673cd5c3d6f712979e
Yash Diniz <26359876+yashdiniz@users.noreply.github.com>
2023-08-21 10:56:27 +0530

    :memo: trying out lsp config

--- nvim/after/plugin/flutter_tools.lua
+++ nvim/after/plugin/flutter_tools.lua
@@ -1,1 +1,7 @@
-require("flutter-tools").setup {} -- use defaults
+require("flutter-tools").setup {
+    lsp = {
+        background_color = { r = 19, g = 17, b = 24 },
+    },
+} -- use defaults
+
+require("telescope").load_extension("flutter")
