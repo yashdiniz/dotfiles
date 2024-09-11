16ee4bb8a292f16b16c213899f08efaaeecc50e6
Yash Diniz <26359876+yashdiniz@users.noreply.github.com>
2024-09-11 21:52:09 +0530

    chore: remove codecompanion and flutter_tools after scripts

--- nvim/after/plugin/codecompanion.lua
+++ nvim/after/plugin/codecompanion.lua
@@ -1,27 +1,27 @@
-local home = vim.fn.expand('$HOME')
-
--- Default config https://github.com/olimorris/codecompanion.nvim?tab=readme-ov-file#wrench-configuration
-require("codecompanion").setup({
-    adapters = {
-        anthropic = require("codecompanion.adapters").use("anthropic", {
-            env = {
-                api_key = "cmd:gpg --decrypt " .. home .. "/anthropic-secret.env.gpg",
-            },
-        }),
-        openai = require("codecompanion.adapters").use("openai", {
-            schema = {
-                model = {
-                    default = "gpt-4o",
-                },
-            },
-            env = {
-                api_key = "cmd:gpg --decrypt " .. home .. "/oai-secret.env.gpg",
-            },
-        }),
-    },
-    strategies = {
-        chat = "openai",
-        inline = "openai",
-        tool = "openai"
-    },
-})
+-- local home = vim.fn.expand('$HOME')
+--
+-- -- Default config https://github.com/olimorris/codecompanion.nvim?tab=readme-ov-file#wrench-configuration
+-- require("codecompanion").setup({
+--     adapters = {
+--         anthropic = require("codecompanion.adapters").use("anthropic", {
+--             env = {
+--                 api_key = "cmd:gpg --decrypt " .. home .. "/anthropic-secret.env.gpg",
+--             },
+--         }),
+--         openai = require("codecompanion.adapters").use("openai", {
+--             schema = {
+--                 model = {
+--                     default = "gpt-4o",
+--                 },
+--             },
+--             env = {
+--                 api_key = "cmd:gpg --decrypt " .. home .. "/oai-secret.env.gpg",
+--             },
+--         }),
+--     },
+--     strategies = {
+--         chat = "openai",
+--         inline = "openai",
+--         tool = "openai"
+--     },
+-- })
--- nvim/after/plugin/flutter_tools.lua
+++ nvim/after/plugin/flutter_tools.lua
@@ -1,13 +1,13 @@
-require("flutter-tools").setup {
-    lsp = {
-        background_color = { r = 19, g = 17, b = 24 },
-    },
-    debugger = {
-        enabled = true,
-        register_configurations = function(_)
-            require("dap").configurations.dart = {}
-        end
-    },
-} -- use defaults
-
-require("telescope").load_extension("flutter")
+-- require("flutter-tools").setup {
+--     lsp = {
+--         background_color = { r = 19, g = 17, b = 24 },
+--     },
+--     debugger = {
+--         enabled = true,
+--         register_configurations = function(_)
+--             require("dap").configurations.dart = {}
+--         end
+--     },
+-- } -- use defaults
+--
+-- require("telescope").load_extension("flutter")
